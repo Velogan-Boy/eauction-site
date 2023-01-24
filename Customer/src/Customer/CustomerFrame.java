2bb39d307fcb1552d3853c5a22b0c0ec2c58a1ed
Velmurugan <velmuruganjeyakumar2002@gmail.com>
2023-01-25 01:06:01 +0530

    Some modifications

--- Customer/src/Customer/CustomerFrame.java
+++ Customer/src/Customer/CustomerFrame.java
@@ -81,7 +81,7 @@ public class CustomerFrame extends JFrame {
 			}
 		}
 		
-		icon2=new ImageIcon("/home/ashish/eclipse-workspace/ProjectImages2/Frame2.jpg");
+		icon2=new ImageIcon("../ProjectImages2/Frame2.jpg");
 		
 		lblCustomer = new JLabel("Customer Portal");
 		lblCustomer.setForeground(new Color(224, 255, 255));
@@ -131,7 +131,7 @@ public class CustomerFrame extends JFrame {
 		contentPane.add(result);
 		result.setVisible(false);
 		
-		icon4=new ImageIcon("/home/ashish/eclipse-workspace/ProjectImages2/a1.jpg");
+		icon4=new ImageIcon("../ProjectImages2/a1.jpg");
 		panel_2.setLayout(null);
 		label_1 = new JLabel(icon4);
 		label_1.setBounds(70, 23, 843, 517);
@@ -149,7 +149,7 @@ public class CustomerFrame extends JFrame {
 		label_2.setBounds(40, 75, 400, 25);
 		label_1.add(label_2);
 		
-		icon3=new ImageIcon("/home/ashish/eclipse-workspace/ProjectImages2/logo.png");
+		icon3=new ImageIcon("../ProjectImages2/logo.png");
 		label=new JLabel(icon3);
 		label.setBounds(85, 3, 140, 100);
 		contentPane.setLayout(null);
